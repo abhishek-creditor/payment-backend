f628c6b9c1725ecdec4931e48037b2172cd79c3c
Mausam Jha <mausam@creditoracademy.com>
2026-02-24 17:14:38 +0530

    added validator for plan creation and added CRUD api in routes,controllers,dao file

---
+++ src/routes/productPlan.routes.js
@@ -0,0 +1,13 @@
+// routes/productPlan.routes.js
+const express = require("express");
+const router = express.Router();
+const productPlanController = require("../controllers/productPlan.controller");
+const {validateCreatePlan} = require("../middleware/validateProductPlan");
+
+router.post("/",validateCreatePlan,productPlanController.createPlan);
+router.get("/", productPlanController.getAllPlans); // ?productName=Course
+router.get("/:id", productPlanController.getPlanById);
+router.put("/:id", productPlanController.updatePlan);
+router.delete("/:id", productPlanController.deletePlan);
+
+module.exports = router;
