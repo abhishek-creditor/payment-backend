6928faacad484d71738464a47a66478a81588361
mohd shavez <mohd.shavez@lmsathena.com>
2026-03-25 17:02:56 +0530

    check referenceId is already PAID in Order table

--- src/middleware/idempotency.js
+++ src/middleware/idempotency.js
@@ -38,8 +38,8 @@ module.exports = async function idempotency(req, res, next) {
   }
 
   // --- CHECK REFERENCE ID ALREADY EXIST RETURN RESPONSE ALREADY PAID ---
-  console.log("[Idempotency] Checking for existing orders with productId and referenceId...");
   const referenceId = req.body?.referenceId || req.body?.orderId;
+  console.log(`[Idempotency] Checking for existing orders with productId [${productId}] and referenceId [${referenceId}]`);
 
   if (productId && referenceId) {
     const existingOrder = await prisma.order.findUnique({
